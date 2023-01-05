2aa5ca4315ea9ede0db4cf17a687d1ee08434e6f
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2023-01-05 12:08:34 -0500

    last changes of logic finished

--- components/LocationInformation.jsx
+++ components/LocationInformation.jsx
@@ -1,7 +1,43 @@
-export default function LocationInformation({}){
-    return(
-        <>
+import { Grid, Typography } from "@mui/material"
+import { Stack } from "@mui/system"
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LanguageIcon from "@mui/icons-material/Language";
+import BusinessIcon from '@mui/icons-material/Business';
 
-        </>
+export default function LocationInformation({location , twitter_username , blog , company}){
+    return(
+        <Grid container>
+            <Grid item xs={6}>
+                <Stack>
+                    <LocationOnIcon />
+                    <Typography>{location}</Typography>
+                </Stack>
+            </Grid>
+            <Grid item xs={6}>
+                <Stack>
+                    <TwitterIcon />
+                    <Typography>
+                        {!twitter_username ? "Not Available" : `@${twitter_username}`}
+                    </Typography>
+                </Stack>
+            </Grid>
+            <Grid item xs={6}>
+                <Stack>
+                    <LanguageIcon />
+                    <Typography>
+                        {!blog ? "Not Available" : blog}
+                    </Typography>
+                </Stack>
+            </Grid>
+            <Grid item xs={6}>
+                <Stack>
+                    <BusinessIcon />
+                    <Typography>
+                        {!company ? "Not Available" : company}
+                    </Typography>
+                </Stack>
+            </Grid>
+        </Grid>
     )
-}
+} 
