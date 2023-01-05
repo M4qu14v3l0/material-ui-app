c65ec8625bc7327c6a44856d6a0edd18b641559d
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2023-01-04 23:57:37 -0500

    changes

--- components/FirstInformation.jsx
+++ components/FirstInformation.jsx
@@ -1,14 +1,16 @@
 import { Typography } from "@mui/material";
 import { Stack } from "@mui/system";
+import PaperInformation from "./PaperInformation";
 
-export default function FirstInformation({name , login , created_at}){
+export default function FirstInformation(dataUser){
     
     return(
         <>
             <Stack>
-                <Typography>{name}</Typography>
-                <Typography>{created_at}</Typography>
+                <Typography>{dataUser.name}</Typography>
+                <Typography>{dataUser.created_at}</Typography>
             </Stack>
+            <PaperInformation {...dataUser}/>
         </>
     )
 }
--- components/PaperInformation.jsx
+++ components/PaperInformation.jsx
@@ -1,7 +1,24 @@
-export default function PaperInformation(){
+import { Paper , Stack , Typography} from "@mui/material"
+
+export default function PaperInformation({public_repos , followers , following}){
     return(
         <>
-            
+            <Paper elevation={3}>
+                <Stack>
+                    <Stack>
+                        <Typography>Repositorios</Typography>
+                        <Typography>{public_repos}</Typography>
+                    </Stack>
+                    <Stack>
+                        <Typography>Seguidores</Typography>
+                        <Typography>{followers}</Typography>
+                    </Stack>
+                    <Stack>
+                        <Typography>Seguidos</Typography>
+                        <Typography>{following}</Typography>
+                    </Stack>
+                </Stack>
+            </Paper>
         </>
     )
 }
